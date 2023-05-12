138c24b36906e1da1a5a7cee8113748cf4b053a3
Grayson Poon <grayson.a.poon@gmail.com>
2023-05-11 17:41:41 -0700

    integrate winston logger as module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,20 +3,36 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { JwtModule } from "@nestjs/jwt";
-// import { secret } from "./utils/constants";
 import { UserModule } from "./modules/user.module";
-
+import { WinstonModule, utilities as winstonUtilities } from "nest-winston";
+import * as winston from "winston";
 import * as dotenv from "dotenv";
 import { DietPlanModule } from "./modules/diet-plan.module";
+
 dotenv.config();
 
 @Module({
 	imports: [
+		WinstonModule.forRoot({
+			transports: [
+				new winston.transports.Console({
+					format: winston.format.combine(
+						winston.format.timestamp(),
+						winston.format.ms(),
+						winstonUtilities.format.nestLike("fitness-diet-plan BACKEND", {
+							colors: true,
+							prettyPrint: true,
+						}),
+					),
+				}),
+			],
+			exitOnError: false,
+		}),
 		MongooseModule.forRoot(process.env.MONGO_DB_URI),
-		// JwtModule.register({
-		// 	secret,
-		// 	signOptions: { expiresIn: "2h" },
-		// }),
+		JwtModule.register({
+			secret: process.env.JWT_SECRET,
+			signOptions: { expiresIn: "2h" },
+		}),
 		UserModule,
 		DietPlanModule,
 	],
